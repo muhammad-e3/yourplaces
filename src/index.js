3fa8fc6d487a60a27b480b1e2c7a0609653dbbaa
muhammad-e3 <muhammadezedn@gmail.com>
2024-06-01 18:48:50 +0300

    Ready to deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 import './index.css';
 import App from './App';
-
-if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 ReactDOM.render(<App />, document.getElementById('root'));
